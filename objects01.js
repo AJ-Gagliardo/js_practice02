671a1403bec130e066061cc615fe04d31e8b630d
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-02-01 00:01:36 -0800

    testing object factory

--- objects01.js
+++ objects01.js
@@ -1,4 +1,6 @@
 
+    
+    /*
     function book(title, author, pages, read){
         this.title = title;
     this.author= author;
@@ -15,4 +17,74 @@ book1 = new book('The Subtle Art of Not Giving a F***', 'Mark Manson', 224, true
 
 
 
+function Player(name, marker) {
+    this.name = name;
+    this.marker = marker;
+    this.sayName = function() {
+      console.log(this.name)
+    };
+  }
+  
+  const player1 = new Player('Antonio', 'X');
+  const player2 = new Player('not Antonio', 'O');
+  player1.sayName();  
+  player2.sayName(); 
 
+
+Player.prototype.sayHello = ()=> console.log('hello I am a player');
+
+// Prototypal inheritance
+
+player1.valueOf();
+
+//Every prototype object inherits from Object.prototype by default.
+
+*/
+
+// ### Recommended method for prototypal inheritance #####
+//Object.getPrototypeOf()  to view the prototype of an object
+//Object.setPrototypeOf() to ‘set’ or mutate it
+
+// function Person(name) {
+//     this.name = name;
+//   }
+  
+//   Person.prototype.sayName = function() {
+//     console.log(`Hello, I'm ${this.name}!`);
+//   };
+  
+//   function Player(name, marker) {
+//     this.name = name;
+//     this.marker = marker;
+//   }
+  
+//   Player.prototype.getMarker = function() {
+//     console.log(`My marker is '${this.marker}'`);
+//   };
+// Object.getPrototypeOf(Player.prototype);
+// Object.setPrototypeOf(Player.prototype, Person.prototype);
+// Object.getPrototypeOf(Player.prototype);
+
+// const player1 = new Player('steve', 'X');
+// const player2 = new Player('also steve', 'O');
+
+// player1.sayName(); // Hello, I'm steve!
+// player2.sayName(); // Hello, I'm also steve!
+
+// player1.getMarker(); // My marker is 'X'
+// player2.getMarker(); // My marker is 'O'
+
+
+// factory fuction
+
+function personFactory(name){
+ return{
+    name,
+    talk(){
+      return `hello i am ${name}`
+    }
+  }
+
+}
+
+const me=personFactory('Anto');
