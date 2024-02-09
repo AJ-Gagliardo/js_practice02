49ba66518e5cb224d2385c51243b798a5eba3f47
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-02-09 15:22:52 -0800

    still practicing npm

--- module_practice1/script.js
+++ module_practice1/script.js
@@ -1,1 +1,15 @@
-import {cloneDeep}from "./node_modules/lodash-es/cloneDeep.js";
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+
+const state = {
+    cart: [
+        {product: 'bread',quantity:5},
+        {product: 'pizza',quantity:5}
+
+    ],
+    user: {loggedIn: true},
+};
+
+const stateClone = Object.assign({}, state);
+console.log(stateClone);
