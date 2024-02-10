5e5dc63a0b84b57b2f66490b37a3402d2d0111d8
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-02-10 11:06:18 -0800

    did another webpack practice to understand the steps better

---
+++ webpack_practice2/src/main.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+
+
+
+const numbers = [1,2,3,4,5];
+
+const sum = _.sum(numbers);
+
+
+console.log('Sum:', sum);
+
+console.log('hi'); // this only updates once I put again npx webpack
