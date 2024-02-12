1a3138ee429604d2f00d9f5f24c612e0c41311ab
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-02-11 16:43:41 -0800

    tring my best on  images and styles on webpack

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,13 +1,15 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './icon.png'
+import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   // Add the image to our existing div.
   const myIcon = new Image();
@@ -15,7 +17,10 @@ function component() {
 
   element.appendChild(myIcon);
 
-    return element;
-  }
-  
-  document.body.appendChild(component());
+ console.log(Data);
+ console.log(Notes);
+
+  return element;
+}
+
+document.body.appendChild(component());
