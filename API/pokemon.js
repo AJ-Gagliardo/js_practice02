b8466187b481567a61a8191b6de12c1d08b7b813
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-03-27 11:40:57 -0700

    following tutorials and playing around with pokemon api to undersand concepts better

--- API/pokemon.js
+++ API/pokemon.js
@@ -1,1 +1,11 @@
-console.log('this will be a pokemon api project')
+
+
+fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+.then(response=> {
+    if(!response.ok){
+        throw new Error('could not fetch resource')
+    };
+    return response.json()
+})
+.then(data => console.log(data.name))
+.catch(error=>console.error(error));
