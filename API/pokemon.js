fc2b161fbe2f51f510dd129441f991ca3d8648ed
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-03-27 11:55:28 -0700

    did a small app to search pokemon sprite with fetch, async, await, I am understanding the concept a lot better with this

--- API/pokemon.js
+++ API/pokemon.js
@@ -1,11 +1,39 @@
 
 
-fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
-.then(response=> {
-    if(!response.ok){
-        throw new Error('could not fetch resource')
-    };
-    return response.json()
-})
-.then(data => console.log(data.name))
-.catch(error=>console.error(error));
+// fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+// .then(response=> {
+//     if(!response.ok){
+//         throw new Error('could not fetch resource')
+//     };
+//     return response.json()
+// })
+// .then(data => console.log(data.name))
+// .catch(error=>console.error(error));
+
+
+async function fetchData(){
+
+    const pokemonName  = document.getElementById('pokemonName').value.toLowerCase();
+console.log(pokemonName);
+    try{
+        const response = await(fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`))
+
+        if(!response.ok){
+            throw new Error('could not feth this resource')
+        }
+
+        const data= await response.json();
+        // console.log(data);
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById('pokemonSprite');
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = 'block';
+
+    }
+    catch(error){
+        console.error(error)
+    }
+}
+
+// fetchData();
