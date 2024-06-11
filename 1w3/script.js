abe3b0b195dda35d463e82fef67243ca41b76872
ajgagliardo <ajgagliardo92@gmail.com>
2024-06-10 22:29:39 -0700

    completed an exercise realated with finding anagrams

--- 1w3/script.js
+++ 1w3/script.js
@@ -1,1 +1,33 @@
 console.log("testing");
+
+// const testing = "testing";
+
+const array = [5,3,8,1,2];
+
+array.sort((a,b) => a-b);
+console.log(array)
+
+const asd = 'asd';
+const splitted = asd.split("");
+splitted.sort();
+console.log(splitted)
+
+
+const text1 = 'asd';
+const text2 = 'dsa';
+
+function checkAnagram(a ,b){
+const arrayA = a.split("");
+arrayA.sort();
+const sortedA = arrayA.sort().join("");
+const arrayB = b.split("");
+arrayB.sort();
+const sortedB = arrayB.sort().join("")
+
+
+console.log(sortedA==sortedB)
+console.log(arrayA);
+console.log(arrayB);
+}
+
+checkAnagram(text1,text2);
