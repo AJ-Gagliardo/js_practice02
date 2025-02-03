185c92612c315dddfbe4606bb3123a9267e5d00b
Antonio Gagliardo <ajgagliardo92@gmail.com>
2025-02-02 22:52:32 -0800

    practicing some basic arrays to avoid forgetting them

---
+++ jsReview/array.js
@@ -0,0 +1,14 @@
+let fruits = ["Apple", "Banana", "Cherry"];
+
+// Accessing Elements
+
+// Print the first element of the fruits array.
+// Print the last element without directly writing its index.
+
+console.log(fruits[0]);
+console.log(fruits[fruits.length - 1]);
+
+// Modifying Elements
+
+// Change "Banana" to "Mango".
+// Add "Orange" at the end of the array.
