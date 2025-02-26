6a122a50b7688430d000fe2fe069b04052429c68
Antonio Gagliardo <ajgagliardo92@gmail.com>
2025-02-25 23:48:00 -0800

    checking some more on sets

--- jsReview/set.js
+++ jsReview/set.js
@@ -8,3 +8,16 @@ console.log(numbers);
 
 let uniqueNumbers = [...new Set(numbers)];
 console.log("unique numbers: ", uniqueNumbers);
+
+let numbers2 = [5, 1, 2, 3, 5, 7, 8, 1, 9];
+
+let individualNumber = [...new Set(numbers2)];
+
+function repeatedNumber() {
+  for (let x of numbers2) {
+    if (individualNumber.includes(x)) {
+      return x;
+    }
+  }
+}
+repeatedNumbers();
