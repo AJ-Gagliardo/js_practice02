285d09839f8867c211bd2eed76bba70a08986f96
Antonio Gagliardo <ajgagliardo27@gmail.com>
2025-09-14 20:10:37 -0700

    just going back to basics to practice some js after a month break from coding

---
+++ basics/001basics.js
@@ -0,0 +1,22 @@
+// Practicing absolute basics. I took a break for 2 months so I want to check material
+
+// Found exercises online / asked ai to give me some to be able to practice
+
+//Create a variable with your name and log:
+// Hello, my name is <your name>.
+console.log("name exercise");
+const name = "Antonio";
+console.log(`Hello, my name is ${name}`);
+
+//Make an array of your 3 favorite foods.
+
+// Print the first and last item.
+
+// Add one more food to the array.
+
+console.log("---array---");
+
+const favFood = ["bulgogi", "ramen", "spaghetti a la bolognesa"];
+console.log(`${favFood[0]}, ${favFood[favFood.length - 1]} `);
+favFood.push("Encebollado");
+console.log(`My favorite food are ${favFood}`);
