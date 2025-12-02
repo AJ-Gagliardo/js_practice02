d6974dfb26024330c7645d776846dfc3d3fd68d2
Antonio Gagliardo <ajgagliardo27@gmail.com>
2025-12-01 22:23:36 -0800

    doing more exercises

--- 03_algorithms/recursion.js
+++ 03_algorithms/recursion.js
@@ -1,18 +1,17 @@
 // learning
 
-/*
 function countdown(n) {
   // 1.- base case
   if (n <= 0) {
     return;
   }
-  // 2.- do something
+  // 2.- work
   console.log(n);
   // 3.-recursive call
   countdown(n - 1);
 }
 
-countdown(5);
+// countdown(5);
 
 // sayHI
 
@@ -26,19 +25,44 @@ function sayHi(times) {
   sayHi(times - 1);
 }
 
-sayHi(5);
+// sayHi(5);
 
-*/
 // count up
 
 function countUp(n) {
-  if (n === 0) {
-    return;
-  }
+  if (n === 0) return;
 
   countUp(n - 1);
 
   console.log(n);
 }
 
-countUp(3);
+// countUp(3);
+
+function countUpMirror(n) {
+  //base case
+  if (n < 0) return;
+
+  //work before recursion
+  console.log(n);
+
+  //recursive call
+  countUpMirror(n - 1);
+  //work after recursion
+  console.log(n);
+}
+
+// countUpMirror(3);
+
+function evenOddPattern(n) {
+  //base call
+  if (n === 0) return;
+  //work before
+  if (n % 2 > 0) console.log(`odd ${n}`);
+  //recursion call
+  evenOddPattern(n - 1);
+  //work after recursion
+  if (n % 2 === 0) console.log(`even ${n}`);
+}
+
+evenOddPattern(4);
