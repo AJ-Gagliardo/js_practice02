0ec0220bcdb4d6bfcaf2cd00ac05a5ad404d3619
Antonio Gagliardo <ajgagliardo27@gmail.com>
2025-12-01 21:54:25 -0800

    doing some recursion exercises

--- 03_algorithms/recursion.js
+++ 03_algorithms/recursion.js
@@ -1,5 +1,6 @@
 // learning
 
+/*
 function countdown(n) {
   // 1.- base case
   if (n <= 0) {
@@ -12,3 +13,32 @@ function countdown(n) {
 }
 
 countdown(5);
+
+// sayHI
+
+function sayHi(times) {
+  if (times === 0) {
+    return console.log("done");
+  }
+
+  console.log(`hi ${times}`);
+
+  sayHi(times - 1);
+}
+
+sayHi(5);
+
+*/
+// count up
+
+function countUp(n) {
+  if (n === 0) {
+    return;
+  }
+
+  countUp(n - 1);
+
+  console.log(n);
+}
+
+countUp(3);
