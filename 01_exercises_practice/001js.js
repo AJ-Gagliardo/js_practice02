5da9cdb3f02f4957b5a80eb64657d9e8f50a20b2
Antonio Gagliardo <ajgagliardo27@gmail.com>
2026-01-02 23:00:41 -0800

    practicing more basic exercises

--- 01_exercises_practice/001js.js
+++ 01_exercises_practice/001js.js
@@ -56,3 +56,86 @@ function countPositives(arr) {
 
 // console.log(countPositives([1, -2, 3, 0, -5])); // 2
 // console.log(countPositives([-1, -2, -3])); // 0
+
+function findMax(arr) {
+  let maxValue = arr[0];
+
+  for (let num of arr) {
+    if (num > maxValue) {
+      maxValue = num;
+    }
+  }
+  return maxValue;
+}
+
+// console.log(findMax([3, 7, 2, 9, 4])); // 9
+// console.log(findMax([-5, -2, -10])); //-2
+
+function findMax2(arr) {
+  return Math.max(...arr);
+}
+
+// console.log(findMax2([3, 7, 2, 9, 4]));
+// console.log(findMax2([-5, -2, -10])); //-2
+
+/*
+Write a function countEvensAndOdds that:
+
+Takes an array of numbers
+
+Returns an object with:
+
+evens: how many even numbers
+
+odds: how many odd numbers
+*/
+function countEvensAndOdds(arr) {
+  let evenAndOdds = { evens: 0, odds: 0 };
+  for (let num of arr) {
+    if (num % 2 === 0) {
+      evenAndOdds.evens++;
+    } else {
+      evenAndOdds.odds++;
+    }
+  }
+  return evenAndOdds;
+}
+
+// console.log(countEvensAndOdds([4, 4, 3, 1]));
+
+function includesNumber(arr) {
+  let newArray = [];
+  for (let num of arr) {
+    if (newArray.includes(num)) {
+      // console.log(`${num} is already in the array`);
+    } else {
+      newArray.push(num);
+    }
+  }
+  return newArray;
+}
+// console.log(includesNumber([1, 2, 3, 2]));
+// console.log(includesNumber([-1, 0, 5]));
+
+/*
+Write a function doubleNumbers that:
+
+Takes an array of numbers
+
+Returns a new array
+
+Each number is multiplied by 2
+
+The original array must NOT be changed
+*/
+
+function doubleNumbers(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(arr[i] * 2);
+  }
+  return newArray;
+}
+
+// console.log(doubleNumbers([1, 2, 3])); // [2, 4, 6]
+// console.log(doubleNumbers([-1, 0, 5])); // [-2, 0, 10]
